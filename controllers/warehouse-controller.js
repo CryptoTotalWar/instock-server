75c1ea905d87f8c5b6aeec2f998f5a432b70ec5b
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-09 14:45:06 -0500

    added function for edit trying

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -11,9 +11,35 @@ const editWarehouse = async () => {
         !contact_email
     ){
         return res.status(400).json({ message: "All fields are required" });
-
     }
-}
+    try {
+        // Update the warehouse data in the database
+        const updatedRows = await knex('warehouse')
+          .where({ id: id })
+          .update({
+            warehouse_name,
+            address,
+            city,
+            country,
+            contact_name,
+            contact_position,
+            contact_phone,
+            contact_email,
+          });
+    
+        if (updatedRows > 0) {
+          // Fetch and send the updated warehouse data
+          const updatedWarehouse = await knex('warehouse').where({ id: id }).first();
+          res.status(200).json(updatedWarehouse);
+        } else {
+          res.status(404).json({ message: `Warehouse with ID ${id} not found` });
+        }
+      } catch (error) {
+        res.status(500).json({ message: `Error updating warehouse: ${error}` });
+      }
+    };
+
+
 const findWarehouse = async (req, res) => {
     try {
         const warehouseFound = await knex('warehouses')
@@ -37,4 +63,5 @@ const findWarehouse = async (req, res) => {
 
 module.exports = {
     findWarehouse,
+    editWarehouse,
 };
