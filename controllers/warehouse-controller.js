3bca19227378fa1e71dceb2ad48641567eaa0ab7
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-09 16:10:28 -0500

    completed function for edit warehouses

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,45 +1,59 @@
 const knex = require('knex')(require('../knexfile'))
 
-const editWarehouse = async () => {
+const editWarehouse = async (req, res) => {
     const { id } = req.params;
-    const {warehouse_name, address, city, country, contact_name, contact_position, contact_phone, contact_email} 
-    = req.body;
-
+    const {
+      warehouse_name,
+      address,
+      city,
+      country,
+      contact_name,
+      contact_position,
+      contact_phone,
+      contact_email,
+    } = req.body;
+  
     if (
-        !id || !warehouse_name || !address || !city ||
-        !country ||  !contact_name ||
-        !contact_position || !contact_phone||
-        !contact_email
-    ){
-        return res.status(400).json({ message: "All fields are required" });
+      !id ||
+      !warehouse_name ||
+      !address ||
+      !city ||
+      !country ||
+      !contact_name ||
+      !contact_position ||
+      !contact_phone ||
+      !contact_email
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+  
     try {
-        // Update the warehouse data in the database
-        const updatedRows = await knex('warehouse')
-          .where({ id: id })
-          .update({
-            warehouse_name,
-            address,
-            city,
-            country,
-            contact_name,
-            contact_position,
-            contact_phone,
-            contact_email,
-          });
-    
-        if (updatedRows > 0) {
-          // Fetch and send the updated warehouse data
-          const updatedWarehouse = await knex('warehouse').where({ id: id }).first();
-          res.status(200).json(updatedWarehouse);
-        } else {
-          res.status(404).json({ message: `Warehouse with ID ${id} not found` });
-        }
-      } catch (error) {
-        res.status(500).json({ message: `Error updating warehouse: ${error}` });
+      // Update the warehouse data in the database
+      const updatedRows = await knex('warehouses')
+        .where({ id: id })
+        .update({
+          warehouse_name,
+          address,
+          city,
+          country,
+          contact_name,
+          contact_position,
+          contact_phone,
+          contact_email,
+        });
+  
+      if (updatedRows > 0) {
+        // Fetch and send the updated warehouse data
+        const updatedWarehouse = await knex('warehouses').where({ id: id }).first();
+        res.status(200).json(updatedWarehouse);
+      } else {
+        res.status(404).json({ message: `Warehouse with ID ${id} not found` });
       }
-    };
-
+    } catch (error) {
+      res.status(500).json({ message: `Error updating warehouse: ${error}` });
+    }
+  };
+  
 
 const findWarehouse = async (req, res) => {
     try {
