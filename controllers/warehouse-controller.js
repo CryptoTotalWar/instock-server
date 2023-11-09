d89fc9342f7dc5951ad6686c93a95975ba031ac4
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-09 13:48:24 -0500

    added knex for the single id in warehouses

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,13 +1,6 @@
 const knex = require('knex')(require('../knexfile'))
 
-const index = async (_req, res) => {
-    try {
-      const data = await knex('warehouses');
-      res.status(200).json(data);
-    } catch(err) {
-      res.status(400).send(`Error retrieving Users: ${err}`)
-    }
-  }
+
 const findWarehouse = async (req, res) => {
     try {
         const warehouseFound = await knex('warehouses')
@@ -15,7 +8,7 @@ const findWarehouse = async (req, res) => {
     
     if (warehouseFound.length === 0) {
         return res.status(404).json({
-          message: `User with ID ${req.params.id} not found` 
+          message: `Warehouse with ID ${req.params.id} not found` 
         });
       }
   
@@ -24,14 +17,11 @@ const findWarehouse = async (req, res) => {
     }   
     catch (error) {
       res.status(500).json({
-        message: `Unable to retrieve user data for user with ID ${req.params.id}`,
+        message: `Unable to retrieve warehouses data for warehouse with ID ${req.params.id}: ${error}`,
       });
     }
 }
-// router.get('/:id', warehouseControler.findWarehouse)
-
 
 module.exports = {
-    index,
     findWarehouse,
 };
