2fc03a2d3b873e03e92328212a85ac9f00812339
CryptoTotalWar <pablo@dltpartners.io>
2023-11-11 18:17:28 -0500

    update res.json(warehouseData) to res.status(200).json(warehouseData) to explicitly return a status code of 200 when sucesfull

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,9 +1,35 @@
 // controllers/warehouse-controller.js
-const knex = require('knex')(require('../knexfile'))
+const knex = require("knex")(require("../knexfile"));
 
 const editWarehouse = async (req, res) => {
-    const { id } = req.params;
-    const {
+  const { id } = req.params;
+  const {
+    warehouse_name,
+    address,
+    city,
+    country,
+    contact_name,
+    contact_position,
+    contact_phone,
+    contact_email,
+  } = req.body;
+
+  if (
+    !id ||
+    !warehouse_name ||
+    !address ||
+    !city ||
+    !country ||
+    !contact_name ||
+    !contact_position ||
+    !contact_phone ||
+    !contact_email
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const updatedRows = await knex("warehouses").where({ id: id }).update({
       warehouse_name,
       address,
       city,
@@ -12,102 +38,76 @@ const editWarehouse = async (req, res) => {
       contact_position,
       contact_phone,
       contact_email,
-    } = req.body;
-  
-    if (
-      !id ||
-      !warehouse_name ||
-      !address ||
-      !city ||
-      !country ||
-      !contact_name ||
-      !contact_position ||
-      !contact_phone ||
-      !contact_email
-    ) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-  
-    try {
-      const updatedRows = await knex('warehouses')
+    });
+
+    if (updatedRows > 0) {
+      const updatedWarehouse = await knex("warehouses")
         .where({ id: id })
-        .update({
-          warehouse_name,
-          address,
-          city,
-          country,
-          contact_name,
-          contact_position,
-          contact_phone,
-          contact_email,
-        });
-  
-      if (updatedRows > 0) {
-        const updatedWarehouse = await knex('warehouses').where({ id: id }).first();
-        res.status(200).json(updatedWarehouse);
-      } else {
-        res.status(404).json({ message: `Warehouse with ID ${id} not found` });
-      }
-    } catch (error) {
-      res.status(500).json({ message: `Error updating warehouse: ${error}` });
+        .first();
+      res.status(200).json(updatedWarehouse);
+    } else {
+      res.status(404).json({ message: `Warehouse with ID ${id} not found` });
     }
-  };
-  
+  } catch (error) {
+    res.status(500).json({ message: `Error updating warehouse: ${error}` });
+  }
+};
 
 const findWarehouse = async (req, res) => {
-    try {
-        const warehouseFound = await knex('warehouses')
-        .where({id: req.params.id});
-    
+  try {
+    const warehouseFound = await knex("warehouses").where({
+      id: req.params.id,
+    });
+
     if (warehouseFound.length === 0) {
-        return res.status(404).json({
-          message: `Warehouse with ID ${req.params.id} not found` 
-        });
-      }
-  
-      const warehouseData = warehouseFound[0];
-      res.json(warehouseData);
-    }   
-    catch (error) {
-      res.status(500).json({
-        message: `Unable to retrieve warehouses data for warehouse with ID ${req.params.id}: ${error}`,
+      return res.status(404).json({
+        message: `Warehouse with ID ${req.params.id} not found`,
       });
     }
-}
+
+    const warehouseData = warehouseFound[0];
+    res.status(200).json(warehouseData);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve warehouses data for warehouse with ID ${req.params.id}: ${error}`,
+    });
+  }
+};
 
 const deleteWarehouse = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const warehouse = await knex('warehouses').where({ id }).first();
+    const warehouse = await knex("warehouses").where({ id }).first();
     if (!warehouse) {
-      return res.status(404).send(); 
+      return res.status(404).send();
     }
 
     await knex.transaction(async (trx) => {
-      await trx('inventories').where({ warehouse_id: id }).del();
-      await trx('warehouses').where({ id }).del();
+      await trx("inventories").where({ warehouse_id: id }).del();
+      await trx("warehouses").where({ id }).del();
     });
 
-    return res.status(204).send(); 
+    return res.status(204).send();
   } catch (error) {
-    
-    return res.status(500).send(); 
+    return res.status(500).send();
   }
 };
 
 const getAllWarehouses = async (req, res) => {
   try {
-    const warehouses = await knex('warehouses').select('*');
+    const warehouses = await knex("warehouses").select("*");
     res.status(200).json(warehouses);
   } catch (error) {
-    res.status(500).json({ message: `Error retrieving warehouses: ${error.message}` });
+    res
+      .status(500)
+      .json({ message: `Error retrieving warehouses: ${error.message}` });
   }
 };
 
 module.exports = {
-    findWarehouse,
-    editWarehouse,
-    getAllWarehouses,
-    deleteWarehouse,
+  findWarehouse,
+  editWarehouse,
+  getAllWarehouses,
+  deleteWarehouse,
 };
