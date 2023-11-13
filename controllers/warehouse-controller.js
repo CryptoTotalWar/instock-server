0b01f748bb7bc1e04f9415585a7ca6fee560a7e4
CryptoTotalWar <pablo@dltpartners.io>
2023-11-13 12:18:29 -0500

    refactored code

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -40,10 +40,8 @@ const editWarehouse = async (req, res) => {
       contact_email,
     });
 
-    if (updatedRows > 0) {
-      const updatedWarehouse = await knex("warehouses")
-        .where({ id: id })
-        .first();
+    if (updatedRows) {
+      const updatedWarehouse = await knex("warehouses").where({ id }).first();
       res.status(200).json(updatedWarehouse);
     } else {
       res.status(404).json({ message: `Warehouse with ID ${id} not found` });
@@ -106,7 +104,6 @@ const getAllWarehouses = async (req, res) => {
 };
 
 const addNewWarehouse = async (req, res) => {
-
   const { id } = req.params;
   const {
     warehouse_name,
@@ -119,7 +116,8 @@ const addNewWarehouse = async (req, res) => {
     contact_email,
   } = req.body;
 
-  if (!warehouse_name ||
+  if (
+    !warehouse_name ||
     !address ||
     !city ||
     !country ||
@@ -134,7 +132,7 @@ const addNewWarehouse = async (req, res) => {
   }
 
   // const phoneInputValidation = /^\\+[1-9]{1}[0-9]{0,2}\([0-9]{0,3}\)[2-9]{1}[0-9]{2}-[0-9]{4}$/;
-  
+
   const phoneInputValidation = /^\+\d{1,3}\s?\(\d{1,4}\)\s?\d{1,10}-?\d{1,10}$/;
 
   if (!contact_phone.match(phoneInputValidation)) {
@@ -143,35 +141,32 @@ const addNewWarehouse = async (req, res) => {
     });
   }
 
-  const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+  const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   if (!contact_email.match(emailValidation)) {
-  return res.status(400).json({
-    message: "Invalid email address",
-  });
-}
+    return res.status(400).json({
+      message: "Invalid email address",
+    });
+  }
 
-try {
-  const result = await knex("warehouses").insert(req.body);
+  try {
+    const result = await knex("warehouses").insert(req.body);
 
-  const newWarehouseId = result[0];
-  const newWarehouse = await knex("warehouses").where({ id: newWarehouseId });
+    const newWarehouseId = result[0];
+    const newWarehouse = await knex("warehouses").where({ id: newWarehouseId });
 
-  res.status(201).json(newWarehouse);
-}
-catch (error) {
-  res.status(500).json({
-    message: `Unable to add new warehouse: ${error}`,
-  });
-
-}
-
-}
+    res.status(201).json(newWarehouse);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to add new warehouse: ${error}`,
+    });
+  }
+};
 
 module.exports = {
   findWarehouse,
   editWarehouse,
   getAllWarehouses,
   deleteWarehouse,
-  addNewWarehouse
+  addNewWarehouse,
 };
